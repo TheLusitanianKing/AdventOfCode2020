1e8119715699e7b5766f0d18035fb682037f9652
AlexLusitania <alex.lisboa.metalhead@gmail.com>
2020-12-20 17:25:21 +0000

    day 17 (part 2)

--- day-17-conway-cubes/src/Cubes.hs
+++ day-17-conway-cubes/src/Cubes.hs
@@ -3,32 +3,39 @@ module Cubes where
 import Data.List (nub)
 import Data.Maybe (mapMaybe)
 
-type Coordinate = (Int, Int, Int)
+type Coordinate = [Int]
 
--- keeping active cubes 3D coordinates only, do not keeping track of the inactive ones
-parseFile :: FilePath -> IO [Coordinate]
-parseFile path = parse . lines <$> readFile path
+-- keeping active cubes coordinates only, do not keeping track of the inactive ones
+parseFile :: Int -> FilePath -> IO [Coordinate]
+parseFile dimension path = parse . lines <$> readFile path
     where parse = doParseMatrix 0 0 []
           doParseMatrix _ _ acc []     = acc
           doParseMatrix x y acc (c:cs) = doParseMatrix 0 (y+1) (doParseRow x y [] c ++ acc) cs
           doParseRow _ _ acc []     = acc
           doParseRow x y acc (c:cs) = doParseRow (x+1) y acc' cs
-            where acc' = if c == '#' then (x, y, 0):acc else acc
+            where acc' = if c == '#' then ([x, y] ++ replicate (dimension - 2) 0):acc else acc
 
--- from a 3D coordinate, gives all 3D coordinates that can be considered neighbours
+-- from a coordinate, gives all coordinates that can be considered neighbours
 neighboursCoordinates :: Coordinate -> [Coordinate]
-neighboursCoordinates (x, y, z) =
+neighboursCoordinates (x:y:z:[]) =
+    map (\(a, b, c) -> [a, b, c])
     [(x', y', z') | x' <- [(x-1)..(x+1)], y' <- [(y-1)..(y+1)], z' <- [(z-1)..(z+1)], (x,y,z) /= (x', y', z')]
+neighboursCoordinates (x:y:z:w:[]) =
+    map (\(a, b, c, d) -> [a, b, c, d])
+    [(x', y', z', w') |
+    x' <- [(x-1)..(x+1)], y' <- [(y-1)..(y+1)], z' <- [(z-1)..(z+1)], w' <- [(w-1)..(w+1)],
+    (x,y,z,w) /= (x', y', z', w')]
+-- TODO: find a better way to do this for all dimensions
 
--- from a 3D coordinate and a list of coordinates where there is an active cube, gives active neighbour cubes
+-- from a coordinate and a list of coordinates where there is an active cube, gives active neighbour cubes
 activeNeighbours :: Coordinate -> [Coordinate] -> [Coordinate]
 activeNeighbours c = filter (\x -> x `elem` neighboursCoordinates c)
 
--- from a 3D coordinate and a list of coordinates where there is an active cube, gives inactive neighbour cubes
+-- from a coordinate and a list of coordinates where there is an active cube, gives inactive neighbour cubes
 inactiveNeighbours :: Coordinate -> [Coordinate] -> [Coordinate]
 inactiveNeighbours c actives = filter (`notElem` actives) (neighboursCoordinates c)
 
--- from a list of 3D coordinates, gives another after cycling once
+-- from a list of coordinates, gives another after cycling once
 cycle :: [Coordinate] -> [Coordinate]
 cycle cs = mapMaybe inactiveF inactiveCoordinates ++ mapMaybe activeF cs
     where inactiveCoordinates = nub $ concatMap (`inactiveNeighbours` cs) cs
--- day-17-conway-cubes/src/Main.hs
+++ day-17-conway-cubes/src/Main.hs
@@ -5,5 +5,7 @@ import Cubes (cycle, parseFile)
 
 main :: IO ()
 main = do
-    activeCubesCoordinates <- parseFile "input.txt"
+    activeCubesCoordinates <- parseFile 3 "input.txt"
     putStrLn . ("part 1: " ++) . show . length $ iterate cycle activeCubesCoordinates !! 6
+    activeCubesCoordinates' <- parseFile 4 "input.txt"
+    putStrLn . ("part 2: " ++) . show . length $ iterate cycle activeCubesCoordinates' !! 6
